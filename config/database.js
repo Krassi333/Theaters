ff273e1c44c737b4c87296ff57ac1dc367f1aec5
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-23 14:28:07 +0300

    Update database configurations

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://127.0.0.1:27017/theaters';
+const connectionString = 'mongodb://127.0.0.1:27017/Theaters';
 
 module.exports = async (app) => {
     try {
