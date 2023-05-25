eed1d37e4af89643e34ae4e3144e96d7827fd386
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-25 22:22:07 +0300

    Add session to configurations

--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
 const handlebars = require('express-handlebars');
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const session = require('../middlewares/session');
 
 module.exports = (app) => {
     const hbs = handlebars.create({
@@ -13,4 +14,5 @@ module.exports = (app) => {
     app.use('/static', express.static('static'));
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
+    app.use(session());
 }
