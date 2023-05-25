8500e184651581f5054ec5c0c4946eb810f495a9
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-25 22:25:01 +0300

    Add session as middleware

--- middlewares/session.js
+++ middlewares/session.js
@@ -1,13 +1,17 @@
+const {verifyToken}=require('../services/authService');
+
 module.exports = () => (req, res, next) => {
     const token = req.cookies.token;
-
+console.log('cookie '+token);
     if (token) {
         try {
             const userData = verifyToken(token);
+            console.log('userData '+userData);
             req.user = userData;
 
 
         } catch (err) {
+            console.log('session err ');
             res.clearCookie('token');
             res.redirect('/auth/login');
         }
