8961554f45a239cd43cc695390ead768872b8d39
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-05-25 22:25:18 +0300

    Create play model

---
+++ models/Play.js
@@ -0,0 +1,20 @@
+const { Schema, model, Types } = require('mongoose');
+const User = require('./User');
+
+const playSchema = new Schema({
+    title: { type: String, required: true, unique: true },
+    description: { type: String, required: true, maxlength: [50, 'Description must be at most 50 characters long!'] },
+    imageUrl: { type: String, required: true },
+    inPublic: { type: Boolean, default: false },
+    createdAt: { type: String, required: true },
+    users: { type: [Types.ObjectId],ref:'User', default: [] }
+});
+
+playSchema.index({title:1},{
+    locale:'en',
+    strength:2
+});
+
+ const Play=model('Play',playSchema);
+
+module.exports=Play;
